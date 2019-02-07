310053bbcd606b61ef7dd37f8980ba265dc3f918
Simon Katz <sk@nomistech.com>
2019-02-07 13:48:39 +0000

    Add some stuff that uses "special" formatting

--- src/nomis_random_playing_clojure_repo/core.clj
+++ src/nomis_random_playing_clojure_repo/core.clj
@@ -1,6 +1,34 @@
 (ns nomis-random-playing-clojure-repo.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;;;; ___________________________________________________________________________
+;;;; ---- do1 ----
+
+(defmacro do1
+  "Evaluates all the forms and returns the result of the first form."
+  {:style/indent 1}
+  [form-1 & other-forms]
+  `(let [result# ~form-1]
+     ~@other-forms
+     result#))
+
+;;;; ___________________________________________________________________________
+;;;; ---- do2 ----
+
+(defmacro do2
+  "Evaluates all the forms and returns the result of the second form."
+  {:style/indent 2}
+  [form-1 form-2 & other-forms]
+  `(do
+     ~form-1
+     (do1
+         ~form-2
+       ~@other-forms)))
+
+;;;; ___________________________________________________________________________
+;;;; ---- econd ----
+
+(defmacro econd
+  "Like `cond`, except throws a RuntimeException if no clause matches."
+  [& clauses]
+  `(cond ~@clauses
+         :else (throw (RuntimeException. "econd has no matching clause"))))
--- test/nomis_random_playing_clojure_repo/core_test.clj
+++ test/nomis_random_playing_clojure_repo/core_test.clj
@@ -1,7 +1,101 @@
 (ns nomis-random-playing-clojure-repo.core-test
-  (:require [clojure.test :refer :all]
-            [nomis-random-playing-clojure-repo.core :refer :all]))
+  (:require [midje.sweet :refer :all]
+            [nomis-random-playing-clojure-repo.core :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;;;; ___________________________________________________________________________
+;;;; ---- sut/do1 ----
+
+(fact "`sut/do1` works"
+
+  (fact "Fails to compile when there are no forms"
+    (macroexpand-1 '(sut/do1))
+    => (throws clojure.lang.ArityException))
+
+  (fact "Returns value of first form when there is one form"
+    (sut/do1 :a)
+    => :a)
+
+  (fact "Returns value of first form when there are two forms"
+    (sut/do1
+        :a
+      :b)
+    => :a)
+
+  (fact "Returns value of first form when there are three forms"
+    (sut/do1
+        :a
+      :b
+      :c)
+    => :a)
+
+  (fact "Forms are evaluated in correct order"
+    (let [side-effect-place (atom [])]
+      (sut/do1
+          (swap! side-effect-place conj 1)
+        (swap! side-effect-place conj 2)
+        (swap! side-effect-place conj 3))
+      => anything
+      (fact
+        @side-effect-place => [1 2 3]))))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/do2 ----
+
+(fact "`sut/do2` works"
+
+  (fact "Fails to compile when there are no forms"
+    (macroexpand-1 '(sut/do2))
+    => (throws clojure.lang.ArityException))
+
+  (fact "Fails to compile when there is one forms"
+    (macroexpand-1 '(sut/do2 :a))
+    => (throws clojure.lang.ArityException))
+
+  (fact "Returns value of second form when there are two forms"
+    (sut/do2
+        :a
+        :b)
+    => :b)
+
+  (fact "Returns value of second form when there are three forms"
+    (sut/do2
+        :a
+        :b
+      :c)
+    => :b)
+
+  (fact "Forms are evaluated in correct order"
+    (let [side-effect-place (atom [])]
+      (sut/do2
+          (swap! side-effect-place conj 1)
+          (swap! side-effect-place conj 2)
+        (swap! side-effect-place conj 3))
+      => anything
+      (fact
+        @side-effect-place => [1 2 3]))))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/econd ----
+
+(fact "`sut/econd` works"
+  (fact "no clauses"
+    (sut/econd)
+    => (throws RuntimeException))
+  (fact "many clauses"
+    (fact "last clause truthy"
+      (sut/econd false 1
+                 nil   2
+                 :this-one 3)
+      => 3)
+    (fact "non-last clause truthy"
+      (sut/econd false 1
+                 nil   2
+                 :this-one 3
+                 :not-this-one 4)
+      => 3)
+    (fact "none truthy"
+      (sut/econd false 1
+                 nil   2
+                 false 3
+                 nil   4)
+      => (throws RuntimeException))))
